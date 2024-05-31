fc9691f28f0f9853fff60a19bc03b88971bdd1e6
bruno-guilherme <dasilvaguilhermebruno@gmail.com>
2024-05-31 11:11:41 -0300

    onboard_finish

--- unicap/semestre-2/residenciaSW/weparty/lib/components/inicio/onboarding/onboarding_items.dart
+++ unicap/semestre-2/residenciaSW/weparty/lib/components/inicio/onboarding/onboarding_items.dart
@@ -4,16 +4,16 @@ class OnboardingItems {
   /// Dados que serão montados a partir da estrutura de dados da classe info.
   
   List<OnboardingInfo> items = [
-    OnboardingInfo(titulo: "titulo1", 
-    descricao: "descricao1", 
-    image: 'assets/images/onboardUm.svg'),
+    OnboardingInfo(titulo: "Bem-vido ao WeParty!", 
+    descricao: "Conectamos você a fornecedores talentosos para transformar suas ideias em realidade", 
+    image: 'assets/images/onboarding/onboard1.svg'),
 
-    OnboardingInfo(titulo: "titulo2", 
-    descricao: "descricao2", 
-    image: 'assets/images/onboardUm.svg'),
+    OnboardingInfo(titulo: "Proporcionamos facilidade!", 
+    descricao: "Simplifique seu planejamento de eventos agora", 
+    image: 'assets/images/onboarding/onboard2.svg'),
 
-    OnboardingInfo(titulo: "titulo3", 
-    descricao: "descricao3", 
-    image: 'assets/images/onboardUm.svg'),
+    OnboardingInfo(titulo: "Encontre seus fornecedores!", 
+    descricao: "Explore perfis de fornecedores, veja avaliações e portfólios em apenas alguns cliques", 
+    image: 'assets/images/onboarding/onboard3.svg'),
   ];
 }
--- unicap/semestre-2/residenciaSW/weparty/lib/components/navigation/pages.dart
+++ unicap/semestre-2/residenciaSW/weparty/lib/components/navigation/pages.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weparty/components/carro/carrinho/cart.dart';
 import 'package:weparty/components/inicio/home_page/home.dart';
-import 'package:weparty/pesquisar/pesquisar.dart';
+import 'package:weparty/components/pesquisar/pesquisar.dart';
 import 'package:weparty/components/conta/profile/profile.dart';
 
 List<Widget> getPages(ThemeData theme) {
